dcc06fdce17f2639e2fc36738e78c4b8550118ab
unknown <nelson.quinones@yahoo.com>
2018-08-05 00:52:17 -0400

    update

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/roommatequest",
   {
-    useMongoClient: false
+    useMongoClient: true
   }
 );
 
